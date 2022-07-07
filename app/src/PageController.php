e89710ba04ab6a7aef425a1ab1cdad63cee26e20
Julian Bennecker <jb@brandcom.de>
2022-07-07 08:25:24 +0200

    Clickjacking: Prevent iframe Inclusion#

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -4,4 +4,11 @@ use SilverStripe\CMS\Controllers\ContentController;
 
 class PageController extends ContentController
 {
+    public function init()
+    {
+        parent::init();
+
+        // https://docs.silverstripe.org/en/4/developer_guides/security/secure_coding/#clickjacking-prevent-iframe-inclusion
+        $this->getResponse()->addHeader('X-Frame-Options', 'SAMEORIGIN');
+    }
 }
