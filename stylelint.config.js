9f53cca43d4909e2f849423dc04368b2965a8a76
Julian Bennecker <jb@brandcom.de>
2023-10-17 13:52:36 +0200

    extend stylelint config to allow tailwind directives

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ["stylelint-config-standard-scss"],
+    rules: {
+        "at-rule-no-unknown": null,
+        "scss/at-rule-no-unknown": [
+            true,
+            {
+                ignoreAtRules: ["tailwind", "apply", "variants", "responsive", "screen"],
+            },
+        ],
+    },
+};
