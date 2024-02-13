7025a39e41af2256c9d81d8b5ad20b62d1c8b8e1
Julian Bennecker <jb@brandcom.de>
2024-02-13 13:27:50 +0100

    Add example properties to TextBaseElement

--- app/src/BaseElements/TextBaseElement.php
+++ app/src/BaseElements/TextBaseElement.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 class TextBaseElement extends BaseElement
 {
+    /**
+     * @link https://silverstripe.github.io/silverstripe-pattern-lib/?path=/story/admin-icons--icon-reference&addons=1&panelRight=0&selectedKind=Admin/Icons&selectedStory=Icon%20reference&stories=1
+     */
+    private static string $icon = "font-icon-p-document";
+
+    private static string $singular_name = "Text Element";
+
+    private static string $plural_name = "Text Elemente";
+
+    private static string $description = "Ein HTML-Editor-Element, zur Darstellung von Text-Inhalten.";
+
     private static array $db = [
         "Content" => "HTMLText",
     ];
