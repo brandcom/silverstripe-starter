3ce70434a19207fe2039b1defcab1ac6b5f77b79
Julian Bennecker <jb@brandcom.de>
2024-02-15 09:50:23 +0100

    german default language

--- app/_config.php
+++ app/_config.php
@@ -2,6 +2,7 @@
 
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 use SilverStripe\View\Requirements;
+use SilverStripe\i18n\i18n;
 
 /*
  * Zwingt alles JavaScript an das Ende des HTMLs
@@ -16,3 +17,12 @@ $contentCSS = $config->getContentCSS();
 $contentCSS[] = "/public/build/admin.min.css";
 $config->setContentCSS($contentCSS);
 $config->insertButtonsAfter("blocks", "styles");
+
+/*
+ * Setzt die Sprache auf Deutsch
+ *
+ * Für mehrsprachige Seiten installiere bitte das Fluent Plugin und entferne diese Zeile.
+ *
+ * @link https://github.com/tractorcow-farm/silverstripe-fluent
+ */
+i18n::set_locale("de_DE");
